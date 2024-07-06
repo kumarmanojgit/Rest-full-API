7992552f7d401236ac844d5ce0c7e454b28bc172
kumarmanojgit <krmanoj72099@gmail.com>
2024-07-06 19:47:08 +0530

    connect mongodb atlas and store thier

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,4 +7,27 @@ exports.userpost = async (req, res) => {
   if (!firstname || !email || !mobile || !gender || !status) {
     res.status(400).json({ error: "All input are required" });
   }
+  try {
+    const preuser = await users.findOne({ email: email });
+    if (preuser) {
+      res.status(400).json({ error: "this email already exit" });
+    } else {
+      //create new date
+      const dateCreate = moment(new Date()).format("YYYY-MM-DD hh:mm:ss");
+      //create new registration
+      const userData = new users({
+        firstname,
+        email,
+        mobile,
+        gender,
+        status,
+        datecreated: dateCreate,
+      });
+      await userData.save();
+      res.status(200).json(userData);
+    }
+  } catch (error) {
+    res.status(400).json(error);
+    console.log("catch block error");
+  }
 };
